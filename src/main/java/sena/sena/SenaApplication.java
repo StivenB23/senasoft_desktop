8a20c075c352ed6038b17038c8741ddc23310cea
Andres Meza <andresmezad2519@gmail.com>
2022-08-11 08:36:08 -0500

    Spring a Dektop

--- src/main/java/sena/sena/SenaApplication.java
+++ src/main/java/sena/sena/SenaApplication.java
@@ -3,11 +3,24 @@ package sena.sena;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.swing.JOptionPane;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+
 @SpringBootApplication
-public class SenaApplication {
+public class SenaApplication implements CommandLineRunner {
+  private static final Logger LOG = LoggerFactory.getLogger(SenaApplication.class);
 
 	public static void main(String[] args) {
+    JOptionPane.showMessageDialog(null, "Hola mi principe");
 		SpringApplication.run(SenaApplication.class, args);
+    LOG.info("SenaApplication stopped");
 	}
+
+  @Override
+  public void run(String... args) throws Exception {
+  }
 
 }
